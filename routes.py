9c42d7d33c57b11eb270cbe8a4b30eef38313f5d
hassan1731996 <hassan1731996@gmail.com>
2022-03-11 16:23:20 +0500

    added changes as per request

--- routes.py
+++ routes.py
@@ -7,4 +7,4 @@ router = APIRouter()
 @router.post("/print-variables/", description="POST two variables from front-end")
 def print_variables_frontend(variables: schemas.FrontendVariables):
     print("variables are here....!!!", variables.name, variables.email)
-    return {"response": "XYZ...!!!!", "variables": variables}
+    return {"submitted_variables": variables, "modified_variable_name": variables.name + " !!!----", }
